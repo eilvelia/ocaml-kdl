f76f49aa1f7cb063e4a551284b2f7102d1a1a062
eilvelia <hi@eilvelia.cat>
2026-03-02 19:53:12 +0000

    shell.nix: use a released version of re2c

--- shell.nix
+++ shell.nix
@@ -1,25 +1,18 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
-  packages = with pkgs; [
+  packages = (with pkgs; [
+    re2c
+  ]) ++ (with pkgs.ocamlPackages; [
     ocaml
     dune_3
-    ocamlPackages.menhir
-    ocamlPackages.menhirLib
-    ocamlPackages.ocaml-lsp
-    ocamlPackages.odoc
-    ocamlPackages.ppx_expect
-    ocamlPackages.sexplib0
-    ocamlPackages.utop
-    ocamlPackages.zarith
 
-    (re2c.overrideAttrs rec {
-      version = "4.1";
-      src = fetchFromGitHub {
-        owner = "skvadrik";
-        repo = "re2c";
-        tag = version;
-        hash = "sha256-xB4oH0QS0VKTK2we+wdylS8VBijpp6tv7YV7fIX1s4A=";
-      };
-    })
-  ];
+    menhir
+    menhirLib
+    ocaml-lsp
+    odoc
+    ppx_expect
+    sexplib0
+    utop
+    zarith
+  ]);
 }
